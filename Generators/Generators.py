bfcc7069bc1a82b8fea4401a1da3dd4ca4987b89
michalina1004 <michalina.owczarek1@gmail.com>
2019-04-20 14:24:07 +0200

    Making class

--- Generators/Generators.py
+++ Generators/Generators.py
@@ -2,336 +2,307 @@ import cmath
 import matplotlib
 import math
 import random
-import numpy as nmp
-import matplotlib.pyplot as plot
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy.stats import norm
+from scipy.stats import shapiro
+from scipy.stats import normaltest
+
+class NormalGen:
+    def __init__(self):
+        pomoc.self=self
+
+    def silnia(n):
+        if n>1:
+            return n*silnia(n-1)
+        else:
+            return 1
+
+    def f_bl(x,n):
+        i=0
+        y=0
+        d=2/(np.sqrt(np.pi))
+        while i<n:
+            y+=d*(((-1)**i)*(x**(2*i+1)))/((2*i+1)*NormalGen.silnia(i))
+            i+=1
+        return y
+
+    def erf_inv(z):
+        w=(np.sqrt(np.pi)/2)*(z+((np.pi*(z**3))/12)+((7*(np.pi**2)*(z**5))/480)+((127*(np.pi**3)*(z**7))/40320)+((4369*(np.pi**4)*(z**9))/5806080))
+        return w
+
+    def fzig(x):
+        return np.e**((-x**2)/2)
 
 
-def silnia(n):
-    if n>1:
-        return n*silnia(n-1)
-    else:
-        return 1
+    def CDF_inversion():
+        x=0.001
+        res=[]
+        while x<1.0:
+            res.append(norm.ppf(x))
+            x+=0.001
+        return(res)
 
-def f_bl(x,n):
-    i=0
-    y=0
-    d=2/(math.sqrt(math.pi))
-    while i<n:
-        y+=d*(((-1)**i)*(x**(2*i+1)))/((2*i+1)*silnia(i))
-        i+=1
-    return y
-
-#erf=[]
-#i=0
-##while i<1:
-    #erf.append([i,f_bl(i,9)])
-    #print(erf[x])
-    #i+=0.0005
-    #x+=1
-
-def erf_inv(z):
-    w=(math.sqrt(math.pi)/2)*(z+((math.pi*(z**3))/12)+((7*(math.pi**2)*(z**5))/480)+((127*(math.pi**3)*(z**7))/40320)+((4369*(math.pi**4)*(z**9))/5806080))
-    return w
-
-#i=0
-#tab=[]
-#cd=[]
-#while i<1:
-    #tab.append(erf_inv(i))
-    #cd.append(norm.ppf(i))
-    #i+=0.05
-#print(tab)
-#print(cd)
-
-def CDF_inversion():
-    x=0.001
-    res=[]
-    while x<1.0:
-        res.append(norm.ppf(x))
-        x+=0.001
-    plot.figure()
-    plot.hist(res)
-    plot.show()
-
-def CDF_raw():
-    x=0
-    res=[]
-    while x<1:
-        res.append(math.sqrt(2)*erf_inv(2*x-1))
-        x+=0.001
-    print(res)
-    plot.figure()
-    plot.hist(res)
-    plot.show()
+    def CDF_raw(n):
+        x=0
+        res=[]
+        while x<1:
+            res.append(np.sqrt(2)*NormalGen.erf_inv(2*x-1))
+            x+=1/n
+        return(res)
 
 
-def CLT1():
-    v=[nmp.random.uniform(0.0,1.0,1) for i in range(12)]
-    z=sum(v)-6
-    print(z)
-    plot.figure()
-    plot.hist(v)
-    plot.show()
+    def CLT(n):
+        res=[]
+        j=0
+        while j<n:
+            v=[np.random.uniform(-0.5,0.5,1) for i in range(20)]
+            z=sum(v)
+            res.append(z[0])
+            j+=1
+        return(res)
 
-def BoxMuller():
-    u1=nmp.random.uniform(0.0,1.0,1000000)
-    u2=nmp.random.uniform(0.0,1.0,1000000)
-    print("log=", nmp.log(u1))
-    a=nmp.sqrt((-2.0)*nmp.log(u1))
-    b=(2.0)*math.pi*u2
-    z1=a*nmp.sin(b)
-    z2=a*nmp.cos(b)
-    plot.figure()
-
-    plot.subplot(221)
-    plot.hist(u1)
-    plot.subplot(222)
-    plot.hist(u2)
-    plot.subplot(223)
-    plot.hist(z1)
-    plot.subplot(224)
-    plot.hist(z2)
-
-    plot.show()
+    def BoxMuller(n):
+        u1=np.random.uniform(0.0,1.0,n)
+        u2=np.random.uniform(0.0,1.0,n)
+        a=np.sqrt((-2.0)*np.log(u1))
+        b=(2.0)*math.pi*u2
+        z1=a*np.sin(b)
+        z2=a*np.cos(b)
+        return(z1,z2)
 
 
-def fc(x,y,a,b):
-    dx=x-a
-    x1=a-dx
-    dy=b-y
-    y1=b+dy
-    return (x1,y1)
-
-def MontyPython1():
-    Gausspdf=nmp.random.uniform(0.0,1.0,1000)
-    print("Gausspdf:\n",Gausspdf)
-    #plot.figure()
-    #bins to pkt (101 pkt)
-    #count to wartości funkcji w pkt (100pkt)
-    count,bins,ignored=plot.hist(Gausspdf,1000)
-    #print("bins:\n",bins,len(bins))
-    #print("count:\n",count,len(count))
-    #plot.show()
-    u1=Gausspdf #all numbers
-    print(u1)
-    print("********************************************************************")
-    print(bins)
-    print("********************************************************************")
-    print(count)
-    print("********************************************************************")
-    s=2.0*nmp.floor(2.0*u1)+1 #+1 is choosing side
-    print("len of u1 ",len(u1)," len of s ", len(s))
-    u2=bins #horizontal component of the uniform 2D random sample
-    b=nmp.sqrt(2/nmp.pi)
-    a=norm.ppf(1/2*b)
-    #print("aaaaaaaaaaaaaaaaaa",a)
-    x=b*u2
-    print("u2",len(u2)," x", len(x))
-    u3=count #vertical
-    print("len(count):",len(u3))
-    res=[]
-    y=[0 for i in range(len(u3))]
-    i=0
-    while i<len(u3):
-        #print("CDF z x",norm.cdf(x[i]))
-        #print("for A area")
-        y[i]=u3[i]/(2.0*b)
-        if x[i]<a:#a[i]
-            if x[i]>=0:
-                res.append(s[i]*x[i])
-        if y[i]<norm.cdf(x[i]):
-            #print("for B area")
-            if x[i]>=0:
-                res.append(s[i]*x[i])
-        if fc(x[i],y[i],a,b)[1]<norm.cdf(x[i]):
-            if fc(x[i],y[i],a,b)[0]>=0:
-                res.append(s[i]*fc(x[i],y[i],a,b)[0])
-            #print("s=",s[i],"   s[i]*fc(x[i],y[i],a,b)[0]=",s[i]*fc(x[i],y[i],a,b)[0])
-        i+=1
-    print("len res", len(res))
-    print(res)
-    plot.figure()
-    plot.hist(res)
-    plot.show()
+    def PolarReject(n):
+        x=2.0*np.random.uniform(-1,1,n) #v1
+        y=2.0*np.random.uniform(-1,1,n) #v2
+        d=x**2+y**2
+        i=0
+        fx,fy=[],[]
+        while(i<n):
+            if(d[i]>0 and d[i]<1):
+                fx.append(x[i]*np.sqrt((-2.0*np.log(d[i]))/d[i]))
+                fy.append(y[i]*np.sqrt((-2.0*np.log(d[i]))/d[i]))
+            i+=1
+        return(fx,fy)
 
 
-def PolarReject():
-    x=2.0*nmp.random.uniform(-1,1,1000000) #v1
-    y=2.0*nmp.random.uniform(-1,1,1000000) #v2
-    d=x**2+y**2
-    print("x=",x,"\ny=",y,"\nd=",d)
-    i=0
-    fx,fy=[],[]
-    while(i<1000):
-        if(d[i]>0 and d[i]<1):
-            fx.append(x[i]*nmp.sqrt((-2.0*nmp.log(d[i]))/d[i]))
-            fy.append(y[i]*nmp.sqrt((-2.0*nmp.log(d[i]))/d[i]))
-        i+=1
-    print("fx:",fx,"\nfy:",fy)
-    plot.figure()
-    plot.subplot(221)
-    plot.hist(fx)
-    plot.subplot(222)
-    plot.hist(fy)
-    plot.show()
 
-
-#nie działa
-def Marsaglia_Bray():
-    s=nmp.random.uniform
-    u=nmp.random.uniform(0.0,1.0,7)
-    #plot.hist(u)
-    #plot.show()
-    a0=0.8638
-    a1=0.1107
-    a2=0.0228002039
-    a3=1-a0-a1-a2
-    s=u
-    res=[]
-    g0=2*(u[0]+u[1]+u[2]-1.5)
-    g1=1.5*(u[3]+u[4]-1)
-    x=6*u[5]-3
-    y=0.358*u[6]
-    g2=norm.cdf(x)-(a0*g0+a1*g1)/(a0+a1+a2)
-    if s[0]<a0:
-        res.append(g0)
-    elif s[0]<a0+a1:
-        res.append(g1)
-    elif s[0]<a0+a1+a2:
-        while y>g2:
-            x=6*u[5]-3
-            y=0.358*u[6]
-        res.append(x)
-    print(res)
-    plot.figure()
-    plot.hist(res)
-    plot.show()
-
-
-def Ratio_of_Uniforms():
-    u=nmp.random.uniform(0.0,1.0,100000)
-    v=nmp.random.uniform(0.0,1.0,100000)
-    i=0
-    res=[]
-    while i<len(u):
-        x=(v[i]*nmp.sqrt(2/nmp.e))/u[i]
-        if x**2<=5-4**(1/4)*u[i]:
-            res.append(x)
-        elif x**2<(4*nmp.e**(-1.35))/u[i]+1.4:
-            if v[i]**2<-4*u[i]**2*nmp.log(u[i]):
+    #wymaga zmian
+    def Marsaglia_Bray(n):
+        s=np.random.uniform(0.0,1.0,n)
+        u=[0 for y in range(n)]
+        for i in range(n):
+            u[i]=np.random.uniform(0,1,7)
+        #print(u)
+        #plt.hist(u)
+        #plt.show()
+        a0=0.8638
+        a1=0.1107
+        a2=0.0228002039
+        a3=1-a0-a1-a2
+        res=[]
+        for i in range(n):
+            g0=2*(u[i][0]+u[i][1]+u[i][2]-1.5)
+            g1=1.5*(u[i][3]+u[i][4]-1)
+            x=6*u[i][5]-3
+            y=0.358*u[i][6]
+            g2=norm.cdf(x)-(a0*g0+a1*g1)/(a0+a1+a2)
+            if s[i]<a0:
+                res.append(g0)
+            elif s[i]<a0+a1:
+                res.append(g1)
+            elif s[i]<a0+a1+a2:
+                k=0
+                while y>g2 and k<2:
+                    x=6*u[i][5]-3
+                    y=0.358*u[i][6]
+                    g2=norm.cdf(x)-(a0*g0+a1*g1)/(a0+a1+a2)
+                    k+=1
                 res.append(x)
-        i+=1
-    print(res)
-    plot.figure()
-    plot.subplot(221)
-    plot.hist(u)
-    plot.subplot(222)
-    plot.hist(v)
-    plot.subplot(223)
-    plot.hist(res)
-    plot.show()
+        print(res)
+        plt.figure()
+        plt.hist(res,1000)
+        plt.show()
 
-def Leva_Ratio():
-    u=nmp.random.uniform(0.0,1.0,1000000)
-    v=nmp.sqrt(2/nmp.e)*nmp.random.uniform(0.0,1.0,1000000)
-    s=0.449871
-    t=-0.386595
-    r1=0.27597
-    r2=0.27846
-    a=0.196
-    b=0.25472
-    i=0
-    res=[]
-    x=[0 for j in range(len(u))]
-    y=[0 for j in range(len(u))]
-    while i<len(u):
-        x[i]=u[i]-s
-        y[i]=nmp.abs(v[i])-t
-        Q=x[i]**2+a*y[i]**2-b*x[i]*y[i]
-        if Q<r1:
-            res.append(v[i]/u[i])
-        elif Q<r2:
-            if v[i]**2<-4*u[i]**2*nmp.log(u[i]):
+
+    def Ratio_of_Uniforms(n):
+        u=np.random.uniform(0.0,1.0,n)
+        v=np.random.uniform(0.0,1.0,n)
+        i=0
+        res=[]
+        while i<n:
+            x=(v[i]*np.sqrt(2/np.e))/u[i]
+            if x**2<=5-4**(1/4)*u[i]:
+                res.append(x)
+            elif x**2<(4*np.e**(-1.35))/u[i]+1.4:
+                if v[i]**2<-4*u[i]**2*np.log(u[i]):
+                    res.append(x)
+            i+=1
+        return(res)
+
+
+        #działa
+    def Leva_Ratio(n):
+        u=np.random.uniform(0.0,1.0,n)
+        v=np.sqrt(2/np.e)*np.random.uniform(0.0,1.0,n)
+        s=0.449871
+        t=-0.386595
+        r1=0.27597
+        r2=0.27846
+        a=0.196
+        b=0.25472
+        i=0
+        res=[]
+        x=[0 for j in range(n)]
+        y=[0 for j in range(n)]
+        while i<n:
+            x[i]=u[i]-s
+            y[i]=np.abs(v[i])-t
+            Q=x[i]**2+a*y[i]**2-b*x[i]*y[i]
+            if Q<r1:
                 res.append(v[i]/u[i])
-        i+=1
-    print(res)
-    plot.figure()
-    plot.subplot(221)
-    plot.hist(u)
-    plot.subplot(222)
-    plot.hist(v)
-    plot.subplot(223)
-    plot.hist(res)
-    plot.show()
+            elif Q<r2:
+                if v[i]**2<-4*u[i]**2*np.log(u[i]):
+                    res.append(v[i]/u[i])
+            i+=1
+        return(res)
 
-def Grand():
-    i=0
-    x=-1
-    while x<0:
-        x=nmp.random.uniform(0.0,1.0,1)
-    print(x)
-    while x<0.5:
-        x=2*x
-        i+=1
-    if x<[1]:
-        print("mniejsze")
-    a=[norm.ppf(1-2**(-j-1)) for j in range(6)]
-    G=[1/2*(x**2-a[j]**2) for j in range(6)]
-    u=(a[i+1]-a[i])*G[1]
-    v=u*(u/2+a[i])
-    while i>-1:
+    def Grand():
+        i=0
+        x=-1
+        while x<0:
+            x=np.random.uniform(0.0,1.0,1)
+        print(x)
+        while x<0.5:
+            x=2*x
+            i+=1
+        if x<[1]:
+            print("mniejsze")
+        a=[norm.ppf(1-2**(-j-1)) for j in range(6)]
+        G=[1/2*(x**2-a[j]**2) for j in range(6)]
         u=(a[i+1]-a[i])*G[1]
         v=u*(u/2+a[i])
-        while v>G[5]:
-            if v<G[2]:
-                if G[3]<0.5:
-                    print(a[i]+u)
-                    return
+        while i>-1:
+            u=(a[i+1]-a[i])*G[1]
+            v=u*(u/2+a[i])
+            while v>G[5]:
+                if v<G[2]:
+                    if G[3]<0.5:
+                        print(a[i]+u)
+                        return
+                    else:
+                        print(-a[i]-u)
+                        return
                 else:
-                    print(-a[i]-u)
-                    return
-            else:
-                v=G[4]
-
-def fzig(x):
-    return nmp.e**((-x**2)/2)
+                    v=G[4]
 
 
-def Ziggurat():
-    n=256
-    #nmp.exp
-    x=[0 for i in range(256)]
-    x[255]=3.6541528853610088 #r
-    v=0.00492867323399
-    for j in range(254,0,-1):
-        x[j]=nmp.sqrt(-2*nmp.log(v/x[j+1]+fzig(x[j+1])))
-    #print(x)
-    j=0
-    res=[]
-    while j<1000:
-        n=nmp.random.randint(0,255,1)
-        u1=nmp.random.uniform(0.0,1.0,1)
-        u2=nmp.random.uniform(0.0,1.0,1)
-        i=int(1+nmp.floor(n*u1)[0])
-        #print(i)
-        z=x[i]*u2
-        if nmp.abs(z)<x[i-1]:
-            res.append(z[0])
-        elif i!=n:
-            y=(fzig(x[i-1])-fzig(x[i]))*nmp.random.uniform(0.0,1.0,1)
-            if y<fzig(z)-fzig(x[i]):
+
+    def Ziggurat(m):
+        n=256
+        x=[0 for i in range(256)]
+        x[255]=3.6541528853610088 #r
+        v=0.00492867323399
+        for j in range(254,0,-1):
+            x[j]=np.sqrt(-2*np.log(v/x[j+1]+NormalGen.fzig(x[j+1])))
+        j=0
+        res=[]
+        while j<m:
+            n=np.random.randint(0,255,1)
+            u1=np.random.uniform(0.0,1.0,1)
+            u2=np.random.uniform(0.0,1.0,1)
+            i=int(1+np.floor(n*u1)[0])
+            z=x[i]*u2
+            if np.abs(z)<x[i-1]:
                 res.append(z[0])
-        j+=1
-    print(res)
-    plot.figure()
-    plot.hist(res)
-    plot.show()
+            elif i!=n:
+                y=(np.e**((-x[i-1]**2)/2)-np.e**((-x[i]**2)/2))*np.random.uniform(0.0,1.0,1)
+                if y<np.e**((-z**2)/2)-np.e**((-x[i]**2)/2):
+                    res.append(z[0])
+            j+=1
+        res1=[]
+        for i in range(len(res)):
+            res1.append(-res[i])
+        res=res+res1
+        return(res)
+
+    def show(method, den):
+        if len(method)>2:
+            plt.figure()
+            plt.hist(method,den,density=True)
+            plt.show()
+        elif len(method)==2:
+            plt.figure()
+            plt.subplot(211)
+            plt.hist(method[0],den,density=True)
+            plt.subplot(212)
+            plt.hist(method[1],den,density=True)
+            plt.show()
+
+    def check_mean_std(method):
+        res=method
+        if len(method)>2:
+            print('mean=%.3f stdv=%.3f' % (np.mean(res), np.std(res)))
+        elif len(method)==2:
+            print('1) mean=%.3f stdv=%.3f' % (np.mean(res[0]), np.std(res[0])))
+            print('2) mean=%.3f stdv=%.3f' % (np.mean(res[1]), np.std(res[1])))
+
+    def check_shapiro(method):
+        if len(method)>2:
+            stat, p = normaltest(method)
+            print('Statistics=%.3f, p=%.3f' % (stat, p))
+            # interpret
+            alpha = 0.05
+            if p > alpha:
+                print('Sample looks Gaussian (fail to reject H0)')
+            else:
+                print('Sample does not look Gaussian (reject H0)')
+
+        elif len(method)==2:
+            stat, p = shapiro(method[0])
+            print('Statistics=%.3f, p=%.3f' % (stat, p))
+            # interpret
+            alpha = 0.05
+            if p > alpha:
+                print('Sample looks Gaussian (fail to reject H0)')
+            else:
+                print('Sample does not look Gaussian (reject H0)')
+            stat, p = shapiro(method[1])
+            print('Statistics=%.3f, p=%.3f' % (stat, p))
+            # interpret
+            alpha = 0.05
+            if p > alpha:
+                print('Sample looks Gaussian (fail to reject H0)')
+            else:
+                print('Sample does not look Gaussian (reject H0)')
+
+    def check_normtest(method):
+        if len(method)>2:
+            stat, p = normaltest(method)
+            print('Statistics=%.3f, p=%.3f' % (stat, p))
+            # interpret
+            alpha = 0.05
+            if p > alpha:
+                print('Sample looks Gaussian (fail to reject H0)')
+            else:
+                print('Sample does not look Gaussian (reject H0)')
+
+        elif len(method)==2:
+            stat, p = normaltest(method[0])
+            print('Statistics=%.3f, p=%.3f' % (stat, p))
+            # interpret
+            alpha = 0.05
+            if p > alpha:
+                print('Sample looks Gaussian (fail to reject H0)')
+            else:
+                print('Sample does not look Gaussian (reject H0)')
+            stat, p = normaltest(method[1])
+            print('Statistics=%.3f, p=%.3f' % (stat, p))
+            # interpret
+            alpha = 0.05
+            if p > alpha:
+                print('Sample looks Gaussian (fail to reject H0)')
+            else:
+                print('Sample does not look Gaussian (reject H0)')
 
 
-
-
-#print(fc(9,2,5,3))
-#MontyPython1()
-CDF_inversion()
-CDF_raw()
